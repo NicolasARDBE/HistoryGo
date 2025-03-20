fe1bd913bfb4dbdfabd841797293abdcf97a99cb
pouyerDev <pouyermejia@gmail.com>
2025-03-20 18:16:09 -0500

    Rama Maps con fragments y pop up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,4 +70,10 @@ dependencies {
 
     //glide (image loading)
     implementation (libs.glide)
+
+    // open street map
+    implementation ("com.google.android.gms:play-services-location:21.2.0")
+    implementation ("com.google.android.gms:play-services-maps:18.2.0")
+    implementation ("com.google.code.gson:gson:2.8.8")
+    implementation("com.github.MKergall:osmbonuspack:6.9.0")
 }
